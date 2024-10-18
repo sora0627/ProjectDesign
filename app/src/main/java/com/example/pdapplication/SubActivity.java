9d9df68e770a1a4564b762fa031086815b3b6a0a
sora0627 <pcsorairo@gmail.com>
2024-10-18 22:41:22 +0900

    add 10/18

--- app/src/main/java/com/example/pdapplication/SubActivity.java
+++ app/src/main/java/com/example/pdapplication/SubActivity.java
@@ -6,20 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity{
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
 
         //ボタンを押したとき
         findViewById(R.id.seni_button01).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //インテントの追加
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
