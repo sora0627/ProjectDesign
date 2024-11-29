524167f0a7fb5691486f4741aa3d3c9509fc2de6
unknown <c1345241@st.kanazawa-it.ac.jp>
2024-11-29 14:44:11 +0900

    2024/11/29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,12 @@ android {
 
 dependencies {
 
+    implementation("org.osmdroid:osmdroid-android:6.1.17")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(files("libs\\mysql-connector-j-9.1.0.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
